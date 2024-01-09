bf2ba1db65ec5b8bff4c54ceb4106da11074f14b
EC2 Default User <ec2-user@ip-10-0-14-95.ec2.internal>
2024-01-09 14:50:35 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new("0000-0001", "modeliphone", "igft678", 256);
+Nokia nokia = new("0000-0002", "modelnokia", "igft679", 64);
+
+iphone.InstalarAplicativo("Twitter");
+
+Console.WriteLine("///////");
+
+
+nokia.InstalarAplicativo("Facebook");
